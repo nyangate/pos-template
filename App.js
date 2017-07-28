83a0e54f92b1d497c6827110dffc87f839abcc1e
robertny <robertny@ondemand.co.ke>
2017-07-28 17:49:29 +0300

    signup

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
- import Login from './src/Login';
+import { StyleSheet, Text, View,ToolbarAndroid } from 'react-native';
+import Login from './src/Login';
+import Signup from './src/Signup';
 export default class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.onActionSelected = this.onActionSelected.bind(this);
+    this.state = {message: "Click here to create your account",isLogin:true};
+    this.linkClicked = this.linkClicked.bind(this);
+
+  }
+
+  onActionSelected(){
+
+  }
+
+  linkClicked(){
+    console.log('login/signup clicked');
+    if(this.state.isLogin){
+      this.setState({isLogin:false,message:"Login"});
+    }else{
+      this.setState({isLogin:true,message:"Click here to create your account"});
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Login/>
-        <Text style={{marginTop:20}} >New to makeswift?</Text>
-         <Text style={{color:'#304ffe', marginTop:20}}>Click here to create your account</Text>
+        {this.state.isLogin? <Login/> : <Signup/>}
+        <Text style={{marginTop:20}} >{this.state.isLogin?'New to makeswift?':''}</Text>
+         <Text onPress={this.linkClicked} style={{color:'#304ffe', marginTop:20}}>{this.state.message}</Text>
       </View>
     );
   }
@@ -19,5 +41,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  page: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
   },
 });
---
+++ src/Signup.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+
+
+export default class Signup extends React.Component{
+
+    constructor(props){
+      super(props);
+      this.sayHi = this.sayHi.bind(this);
+      this.state = {email:"email",password:""}
+    }
+    sayHi(){
+      console.log("Hello there");
+    }
+
+  render(){
+    return (<View>
+      <Text style={{width: 250, height: 40, marginBottom:10, textAlign:'center', fontSize:20}}>Get started</Text>
+      <Text>Full Name</Text>
+      <TextInput autoCorrect={false} underlineColorAndroid='rgba(0,0,0,0)'
+          style={{width: 250, height: 40, marginBottom:10, marginTop: 5, backgroundColor: 'white', borderColor: 'gray', borderWidth: 1, borderRadius:5}}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+      <Text>Email</Text>
+      <TextInput autoCorrect={false} underlineColorAndroid='rgba(0,0,0,0)'
+          style={{width: 250, height: 40, marginBottom:10, marginTop: 5, backgroundColor: 'white', borderColor: 'gray', borderWidth: 1, borderRadius:5}}
+          onChangeText={(text) => this.setState({text})}
+          value={this.state.text}
+        />
+        <Text style={{width: 250, marginBottom:5, marginTop: 5}}>Phone</Text>
+        <TextInput autoCorrect={false} underlineColorAndroid='rgba(0,0,0,0)'
+            style={{width: 250, height: 40, borderBottomColor:'white', marginBottom:20, marginTop: 5, backgroundColor: 'white', borderColor: 'gray', borderWidth: 1,borderRadius:5}}
+            onChangeText={(text) => this.setState({text})}
+            value={this.state.text}
+          />
+          <Text style={{width: 250, marginBottom:5, marginTop: 5}}>Password</Text>
+          <TextInput autoCorrect={false} underlineColorAndroid='rgba(0,0,0,0)'
+              style={{width: 250, height: 40, borderBottomColor:'white', marginBottom:20, marginTop: 5, backgroundColor: 'white', borderColor: 'gray', borderWidth: 1,borderRadius:5}}
+              onChangeText={(text) => this.setState({text})}
+              value={this.state.text}
+            />
+      <Button style={{marginTop: 15}} onPress={this.sayHi}  title="Create Account"
+                color="#841584" accessibilityLabel="Save content"/>
+                </View>
+
+
+)
+  }
+}
